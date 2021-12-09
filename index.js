b15674c264017aa2c2a27347f4aee672365049fb
Daniel Kneale <82185529+Crimson-Beast@users.noreply.github.com>
2021-12-08 17:31:16 -0800

    api push

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/", (req,res) =>{
+    res.json({ data: "api data SERVED" })
+})
+
+app.listen(port, ()=> {
+    console.log(`Server is running on ${port}`)
+})
+
+
+
+
+
+
+
+// console.log("it works")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME) //environment variables
+// console.log(process.env.APIKEY)
+// console.log(process.env.PORT)
+// console.log(process.env.DBPASS)
